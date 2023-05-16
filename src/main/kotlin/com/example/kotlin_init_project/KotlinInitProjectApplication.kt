71d285e8eab9b85ec799d0f18679d337c4557f35
injinjeong <snowwwwww@naver.com>
2023-05-16 11:49:29 +0900

    first commit

---
+++ src/main/kotlin/com/example/kotlin_init_project/KotlinInitProjectApplication.kt
@@ -0,0 +1,10 @@
+package com.example.kotlin_init_project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KotlinInitProjectApplication
+
+fun main(args: Array<String>) {
+
+}
