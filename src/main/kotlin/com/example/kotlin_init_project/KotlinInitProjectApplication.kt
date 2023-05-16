66448f600d33cb611d4ff4aa70578d0ca4a78945
injinjeong <snowwwwww@naver.com>
2023-05-16 11:52:31 +0900

    json, coroutines

--- src/main/kotlin/com/example/kotlin_init_project/KotlinInitProjectApplication.kt
+++ src/main/kotlin/com/example/kotlin_init_project/KotlinInitProjectApplication.kt
@@ -1,10 +1,14 @@
 package com.example.kotlin_init_project
 
+import com.example.kotlin_init_project.examplecode.AsyncTest
+import com.example.kotlin_init_project.examplecode.JsonTest
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
 class KotlinInitProjectApplication
 
 fun main(args: Array<String>) {
+//    JsonTest().jsonParser()
+    AsyncTest().asyncFun()
 
 }
---
+++ src/main/kotlin/com/example/kotlin_init_project/examplecode/JsonTest.kt
@@ -0,0 +1,49 @@
+package com.example.kotlin_init_project.examplecode
+
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+
+class JsonTest {
+    fun jsonParser() {
+        val jsonString = """
+        {
+            "person": [
+                {
+                    "id": 0,
+                    "name": "Mathews Parker",
+                    "email": "goodsleep@aseep.com"
+                },
+                {
+                    "id": 1,
+                    "name": "json as",
+                    "email": "123dslfffp@aseep.com"
+                },
+                {
+                    "id": 2,
+                    "name": "json a123",
+                    "email": "12a23sleep@aseep.com"
+                }
+            ]
+        }
+    """.trimIndent()
+
+        val gson = Gson()
+        val gsonObj = gson.fromJson(jsonString, JsonObject::class.java)
+        val jsonArray = gsonObj.getAsJsonArray("person")
+
+        val persons = jsonArray.map {
+            gson.fromJson(it, Person::class.java)
+        }
+        println("persons = ${persons}")
+        println("-----------------------")
+        for (person in persons) {
+            println("ID: ${person.id}, Name: ${person.name}, Email: ${person.email}")
+        }
+    }
+}
+
+data class Person(
+    val id: Int,
+    val name: String,
+    val email: String
+)
